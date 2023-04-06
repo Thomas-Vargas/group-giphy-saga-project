afeabb4e90c06f09ec44096b3acfdde2f2f1b235
Thomas-Vargas <tavargas2012irc@gmail.com>
2023-04-06 14:59:12 -0500

    Mapping through FavoritesList.

--- src/components/FavoritesView/FavoritesList.jsx
+++ src/components/FavoritesView/FavoritesList.jsx
@@ -4,10 +4,16 @@ import FavoritesItem from "../FavoritesItem/FavoritesItem";
 
 const FavoritesView = () => {
   // useSelector to get favorites list from store
-
+  const FavoritesList = useSelector(store => store.FavoritesList);
   return (
     <>
       {/* map through favorites list and render to dom with FavoritesItem component */}
+      {/* {favoritesList ? 
+      favoritesList.map(favorite => (
+        <div></div>
+      )) : 
+      <div>Loading</div>
+    } */}
     </>
   )
 }
