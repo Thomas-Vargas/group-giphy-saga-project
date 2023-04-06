cb132b718423912a9b347ea384885efffd6e0b7a
Ryan Weispfenning <weisp014@gmail.com>
2023-04-06 14:57:47 -0500

    added heart icon for favorite button

--- src/GifItem/GifItem.jsx
+++ src/GifItem/GifItem.jsx
@@ -1,24 +1,27 @@
 import { useDispatch } from "react-redux";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
-function GifItem({item}) {
-    const dispatch = useDispatch();
-    console.log(item);
-    // TODO: add gif to favorites
-    const favoriteGif = (item) => {
-        console.log('in favoriteGif()');
-        // dispatch to sagas with gif (name and url)
-        dispatch({
-            type: "ADD_TO_FAVORITES",
-            payload: item
-        });
-    }
+function GifItem({ item }) {
+  const dispatch = useDispatch();
+  console.log(item);
+  // TODO: add gif to favorites
+  const favoriteGif = (item) => {
+    console.log("in favoriteGif()");
+    // dispatch to sagas with gif (name and url)
+    dispatch({
+      type: "ADD_TO_FAVORITES",
+      payload: item,
+    });
+  };
 
-    return(
-        <div>
-            <img src={item.images.original.url} />
-            <button className="favBtn" onClick={() => favoriteGif(item)}>FAVORITE</button>
-        </div>
-    )
+  return (
+    <div>
+      <img src={item.images.original.url} />
+      <FavoriteIcon className="favBtn" onClick={() => favoriteGif(item)}>
+        FAVORITE
+      </FavoriteIcon>
+    </div>
+  );
 }
 
 export default GifItem;
