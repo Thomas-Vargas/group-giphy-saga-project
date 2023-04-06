98459fe296177b8540f87fffaabf0ed7e4c59ab2
Fue Lee <fueclee.12@gmail.com>
2023-04-06 14:53:19 -0500

    fully implemented search form input to search and get updated gifs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,22 +2,19 @@ import React from 'react';
 import GifList from '../GifList/GifList';
 import { useEffect } from "react";
 import { useDispatch } from 'react-redux';
+import SearchForm from '../SearchForm/SearchForm';
 
 function App(props) {
 
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch({
-      type: "SEARCH_INPUT",
-      //add later
-    })
-  }, [])
+
   return (
     <>
     <div>
       <h1>Giphy Search!</h1>
     </div>
+    <SearchForm />
     <GifList />
     </>
   );
